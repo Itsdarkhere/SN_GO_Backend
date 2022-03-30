1d01018d6778b055ec59c03b7dac20e57910fcd1
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-03-30 19:21:18 +0300

    get created nfts fixed

--- routes/supernova.go
+++ routes/supernova.go
@@ -3086,6 +3086,181 @@ func (fes *APIServer) GetTrendingAuctions(ww http.ResponseWriter, req *http.Requ
 		conn.Release()
 	}
 }
+type GetProfileNFTsRequest struct {
+	ReaderPublicKeyBytes string 
+	Username string
+	Offset int64
+}
+func (fes *APIServer) GetCreatedNFTs(ww http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(io.LimitReader(req.Body, MaxRequestBodySizeBytes))
+	requestData := GetProfileNFTsRequest{}
+	if err := decoder.Decode(&requestData); err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Error parsing request body: %v", err))
+		return
+	}
+
+	var readerPublicKeyBytes []byte
+	var err error
+	if requestData.ReaderPublicKeyBytes != "" {
+		readerPublicKeyBytes, _, err = lib.Base58CheckDecode(requestData.ReaderPublicKeyBytes)
+		if err != nil {
+			_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Problem decoding ReaderPublicKeyBytes: %v", err))
+			return
+		}
+	}
+
+	if requestData.Username == "" {
+		_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: No Username sent in request"))
+		return
+	}
+
+	var offset int64
+	if requestData.Offset >= 0 {
+		offset = requestData.Offset
+	} else {
+		offset = 0
+	}
+
+	// Get connection pool
+	dbPool, err := CustomConnect()
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Error getting pool: %v", err))
+		return
+	}
+	// get connection to pool
+	conn, err := dbPool.Acquire(context.Background())
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Error cant connect to database: %v", err))
+		conn.Release()
+		return
+	}
+
+	// Release connection once function returns
+	defer conn.Release();
+
+	// Minus one week in Nanos
+    timeUnix := uint64(time.Now().UnixNano()) - 259200000000000
+
+	rows, err := conn.Query(context.Background(),
+	fmt.Sprintf(`SELECT like_count, diamond_count, comment_count, encode(post_hash, 'hex') as post_hash, 
+	poster_public_key, body, timestamp, hidden, repost_count, quote_repost_count, 
+	pinned, nft, num_nft_copies, unlockable, creator_royalty_basis_points,
+	coin_royalty_basis_points, num_nft_copies_for_sale, num_nft_copies_burned, extra_data FROM pg_posts
+	INNER JOIN pg_profiles ON 
+	poster_public_key = public_key WHERE extra_data->>'isEthereumNFT' = 'dHJ1ZQ==' OR nft = true AND username = '%v' 
+	ORDER BY timestamp desc OFFSET %v LIMIT 20`, requestData.Username, offset))
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Error query failed: %v", err))
+		return
+	} else {
+
+		var posts []*PostResponse
+
+		// Defer closing rows
+		defer rows.Close()
+
+        // Next prepares the next row for reading.
+        for rows.Next() {
+			// New post to insert values into
+			post := new(PostResponse)
+			// Body is weird in db so I need this to parse it
+			body := new(Body)
+			// Need a holder var for the bytea format
+			poster_public_key_bytea := new(PPKBytea)
+            // Scan reads the values from the current row into tmp
+            rows.Scan(&post.LikeCount, &post.DiamondCount, &post.CommentCount, &post.PostHashHex, 
+				&poster_public_key_bytea.Poster_public_key, &body.Body, &post.TimestampNanos, &post.IsHidden, &post.RepostCount, 
+				&post.QuoteRepostCount, &post.IsPinned, &post.IsNFT, &post.NumNFTCopies, &post.HasUnlockable,
+				&post.NFTRoyaltyToCoinBasisPoints, &post.NFTRoyaltyToCreatorBasisPoints, &post.NumNFTCopiesForSale,
+				&post.NumNFTCopiesBurned, &post.PostExtraData)
+				// Check for errors
+				if rows.Err() != nil {
+					// if any error occurred while reading rows.
+					_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Error scanning to struct: %v", err))
+					return
+				}
+			if post.PostExtraData["name"] != "" {
+				post.PostExtraData["name"] = base64Decode(post.PostExtraData["name"])
+			}
+			if post.PostExtraData["properties"] != "" {
+				post.PostExtraData["properties"] = base64Decode(post.PostExtraData["properties"])
+			}
+			if post.PostExtraData["category"] != "" {
+				post.PostExtraData["category"] = base64Decode(post.PostExtraData["category"])
+			}
+			if post.PostExtraData["Node"] != "" {
+				post.PostExtraData["Node"] = base64Decode(post.PostExtraData["Node"])
+			}
+			if post.PostExtraData["arweaveVideoSrc"] != "" {
+				post.PostExtraData["arweaveVideoSrc"] = base64Decode(post.PostExtraData["arweaveVideoSrc"])
+			}
+			if post.PostExtraData["arweaveAudioSrc"] != "" {
+				post.PostExtraData["arweaveAudiooSrc"] = base64Decode(post.PostExtraData["arweaveAudioSrc"])
+			}
+			if post.PostExtraData["arweaveModelSrc"] != "" {
+				post.PostExtraData["arweaveModelSrc"] = base64Decode(post.PostExtraData["arweaveModelSrc"])
+			}
+			// Now break down the faulty body into a few parts
+			content := JsonToStruct(body.Body)
+			post.Body = content.Body
+			post.ImageURLs = content.ImageURLs
+			post.VideoURLs = content.VideoURLs
+
+			// Get utxoView
+			utxoView, err := fes.backendServer.GetMempool().GetAugmentedUniversalView()
+			if err != nil {
+				_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Error getting utxoView: %v", err))
+				return
+			}
+
+			// PKBytes to PKID
+			var posterPKID *lib.PKID
+			posterPKID = utxoView.GetPKIDForPublicKey(poster_public_key_bytea.Poster_public_key).PKID
+
+			// PKID to profileEntry and PK
+			profileEntry := utxoView.GetProfileEntryForPKID(posterPKID)
+			var profileEntryResponse *ProfileEntryResponse
+			var publicKeyBase58Check string
+			if profileEntry != nil {
+				profileEntryResponse = fes._profileEntryToResponse(profileEntry, utxoView)
+				publicKeyBase58Check = profileEntryResponse.PublicKeyBase58Check
+			} else {
+				publicKey := utxoView.GetPublicKeyForPKID(posterPKID)
+				publicKeyBase58Check = lib.PkToString(publicKey, fes.Params)
+			}
+			// Assign it to the post being returned
+			post.PosterPublicKeyBase58Check = publicKeyBase58Check
+			// Assign ProfileEntryResponse
+			post.ProfileEntryResponse = profileEntryResponse
+			// Decode the postHash.
+			postHash, err := GetPostHashFromPostHashHex(post.PostHashHex)
+			if err != nil {
+				_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: %v", err))
+				return
+			}
+			// Fetch the postEntry requested.
+			postEntry := utxoView.GetPostEntryForPostHash(postHash)
+			if postEntry == nil {
+				_AddBadRequestError(ww, fmt.Sprintf("GetCreatedNFTs: Could not fetch postEntry"))
+				return
+			}
+			// Get info regarding the readers interactions with the post
+			post.PostEntryReaderState = utxoView.GetPostEntryReaderState(readerPublicKeyBytes, postEntry)
+			// Append to array for returning
+			posts = append(posts, post)
+        }
+
+		resp := PostResponses {
+			PostEntryResponse: posts,
+		}
+		if err = json.NewEncoder(ww).Encode(resp); err != nil {
+			_AddInternalServerError(ww, fmt.Sprintf("GetCreatedNFTs: Problem serializing object to JSON: %v", err))
+			return
+		}
+		// Just to make sure
+		conn.Release()
+	}
+}
 func (fes *APIServer) GetRecentSales(ww http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(io.LimitReader(req.Body, MaxRequestBodySizeBytes))
 	requestData := GetNFTShowcaseRequest{}
